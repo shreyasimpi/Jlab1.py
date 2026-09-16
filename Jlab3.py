d5c27251b6dad04b62fd292d2b4253a838b948f4
shreya <shreyasimpi0704@gmail.com>
2026-09-16 16:12:39 +0530

    save

---
+++ Jlab3.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+a, b = b, a
+print("After swapping:")
+print("a =", a)
+print("b =", b)
